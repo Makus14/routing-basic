f5707c76818f6556ad20c130bb954eddfcd55443
Makus14 <mesha.maksim14@gmail.com>
2024-12-12 16:47:36 +0300

    add links for dynamic routers

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error.js";
+import ProductDetailPage from "./pages/ProductDetail.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/products", element: <ProductsPage /> },
+      { index: true, element: <HomePage /> }, //path: ''
+      { path: "products", element: <ProductsPage /> },
+      { path: "products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
